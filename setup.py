67cb17f082aeae6e56b63daf2f71d08caf7bd30f
harke <22569641+ashimb9@users.noreply.github.com>
2018-07-15 00:20:48 -0500

    MAINT: Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="missingpy",
-    version="0.1.0",
+    version="0.1.1",
     author="Epsilon Machine",
     description="Missing Data Imputation for Python",
     long_description=long_description,
